7e5764a96546a1e8caefb05aad47d623391214c9
Ahmedabdulhamid <109865990+Ahmedabdulhamid@users.noreply.github.com>
2022-11-29 21:11:27 +0300

    Add files via upload

---
+++ 5.js
@@ -0,0 +1,16 @@
+var tex_1 = document.querySelector('.tex-1')
+var btn = document.querySelector('.btn')
+var tex_2 = document.querySelector('.tex-2')
+var btn_1 = document.querySelector('.btn-1')
+btn.onclick = function(e) {
+    if (e.target.classList.contains('a')) {
+        tex_1.classList.toggle('tex-1')
+
+    }
+}
+btn_1.onclick = function(e) {
+    if (e.target.classList.contains('a-1')) {
+        tex_2.classList.toggle('tex-2')
+
+    }
+}
